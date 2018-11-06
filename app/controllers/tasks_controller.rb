091491fcd62e1f157b9cf2b3b0ac94d78ca9c50f
Gareth Phillips <gareth326771@hotmail.com>
2018-11-06 15:26:23 +0000

    basics of TM done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
+
+
